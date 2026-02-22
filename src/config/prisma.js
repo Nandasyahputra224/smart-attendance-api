5bbf194d672d50b4cb4943b59acbf65c39d11d1c
Nandayo <nsyahputra224@gmail.com>
2026-02-22 14:54:56 +0700

    Add database connection checking code

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -3,4 +3,16 @@ import { PrismaClient } from "@prisma/client";
 /** @type {import('@prisma/client').PrismaClient} */
 const prisma = new PrismaClient();
 
+async function checkDbConnect() {
+  try {
+    await prisma.$connect();
+    console.log("✅ Database connected successfully");
+  } catch (err) {
+    console.error("❌ Database connection failed:", err.message);
+    process.exit(1); // matikan server jika database gagal konek
+  }
+}
+
+checkDbConnect();
+
 export default prisma;
